9bb171f383c82938aff6e662143459a6ed068dfd
Swaggeroo <mugel849@gmail.com>
2024-06-01 23:32:09 +0200

    Brightness and Speed endpoint

--- routes/applyImage.ts
+++ routes/applyImage.ts
@@ -15,6 +15,8 @@ const MIN_SPEED: number = 0;
 const MAX_SPEED: number = 60000;
 
 let applyingImage: boolean = false;
+let currentBrightness: number = BRIGHTNESS;
+let currentSpeed: number = SPEED;
 
 router.post('/apply/:uuid', async (req, res) => {
     if (!isConnected()) {
@@ -27,29 +29,6 @@ router.post('/apply/:uuid', async (req, res) => {
     applyingImage = true;
 
     const uuid = req.params.uuid;
-    let brightness: number = Number(req.query.brightness);
-    let speed: number = Number(req.query.speed);
-
-    if (isNaN(brightness)) {
-        brightness = BRIGHTNESS;
-    }else {
-        if (brightness < MIN_BRIGHTNESS) {
-            brightness = MIN_BRIGHTNESS;
-        }else if (brightness > MAX_BRIGHTNESS) {
-            brightness = MAX_BRIGHTNESS;
-        }
-    }
-
-    if (isNaN(speed)) {
-        speed = SPEED;
-    }else {
-        if (speed < MIN_SPEED) {
-            speed = MIN_SPEED;
-        }else if (speed > MAX_SPEED) {
-            speed = MAX_SPEED;
-        }
-    }
-
 
     let picture = await Picture.findOne({uuid: uuid}).select('data animated');
 
@@ -58,10 +37,8 @@ router.post('/apply/:uuid', async (req, res) => {
         return res.status(404).send('The image was not found.');
     }
 
-    await sendBrightnessToMatrix(brightness);
-
     if (picture.animated){
-        await sendAnimationToMatrix(picture.data, speed, res);
+        await sendAnimationToMatrix(picture.data, res);
     }else{
         await sendPictureToMatrix(picture.data[0], res);
     }
@@ -69,6 +46,56 @@ router.post('/apply/:uuid', async (req, res) => {
     applyingImage = false;
     debugApplyImage('The image was applied.');
  });
+
+router.post('/setBrightness', async (req, res) => {
+    let brightness: number = Number(req.query.brightness);
+
+    if (isNaN(brightness)) {
+        return res.status(400).send('Brightness is not a number.');
+    }
+
+    if (brightness < MIN_BRIGHTNESS) {
+        brightness = MIN_BRIGHTNESS;
+    }else if (brightness > MAX_BRIGHTNESS) {
+        brightness = MAX_BRIGHTNESS;
+    }
+
+    try {
+        await sendBrightnessToMatrix(brightness);
+    }catch (err){
+        debugApplyImage('Error sending brightness to matrix: ' + err);
+        return res.status(500).send('Error sending brightness to matrix.');
+    }
+
+    currentBrightness = brightness;
+
+    res.send('Brightness set to ' + brightness);
+});
+
+router.post('/setSpeed', async (req, res) => {
+    let speed: number = Number(req.query.speed);
+
+    if (isNaN(speed)) {
+        return res.status(400).send('Speed is not a number.');
+    }
+
+    if (speed < MIN_SPEED) {
+        speed = MIN_SPEED;
+    }else if (speed > MAX_SPEED) {
+        speed = MAX_SPEED;
+    }
+
+    try {
+        await sendSpeedToMatrix(speed);
+    }catch (err){
+        debugApplyImage('Error sending speed to matrix: ' + err);
+        return res.status(500).send('Error sending speed to matrix.');
+    }
+
+    currentSpeed = speed;
+
+    res.send('Speed set to ' + speed);
+});
 
 async function sendPictureToMatrix(body: string, res: any){
     res.write('0 of 1\n');
@@ -93,10 +120,10 @@ async function sendPictureToMatrix(body: string, res: any){
     res.end();
 }
 
-async function sendAnimationToMatrix(body: string[], speed: number, res: any){
+async function sendAnimationToMatrix(body: string[], res: any){
     try {
         for (let i = 0; i < body.length; i++){
-            body[i] = body[i].replace('-1', speed.toString());
+            body[i] = body[i].replace('-1', currentSpeed.toString());
             res.write(i + ' of ' + body.length + '\n');
             await fetch(MATRIX_URL + '/setAnimationFragment', {
                 method: 'POST',
@@ -119,6 +146,15 @@ async function sendAnimationToMatrix(body: string[], speed: number, res: any){
 async function sendBrightnessToMatrix(brightness: number){
     let body = "{\"brightness\":" + brightness + "}";
     await fetch(MATRIX_URL + '/setBrightness', {
+        method: 'POST',
+        body: body,
+        headers: {'Content-Type': 'application/json'}
+    });
+}
+
+async function sendSpeedToMatrix(speed: number){
+    let body = "{\"frameDelay\":" + speed + "}";
+    await fetch(MATRIX_URL + '/setSpeed', {
         method: 'POST',
         body: body,
         headers: {'Content-Type': 'application/json'}
