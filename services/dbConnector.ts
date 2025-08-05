61a10e71eb79a16b135b1e83ef60dd06adeafa45
Swaggeroo <mugel849@gmail.com>
2025-08-05 21:15:52 +0200

    Improve MongoDB connection handling
    Introduced `connecting` flag to prevent redundant connection attempts. Added logic to disconnect existing connections before initiating a new one for better stability and error handling.

--- services/dbConnector.ts
+++ services/dbConnector.ts
@@ -3,6 +3,7 @@ const debugDB = require('debug')('app:db');
 
 let db = mongoose.connection;
 let connected = false;
+let connecting = false;
 
 //reconnect on disconnect
 db.on('disconnected', function() {
@@ -20,7 +21,12 @@ process.on('SIGINT', function() {
     });
 });
 
-function connect(){
+async function connect(){
+    if (connected || connecting) {
+        debugDB('Already connected or connecting to MongoDB. Skipping connect call.');
+        return;
+    }
+    connecting = true;
     const user = process.env.MONGO_USER;
     const password = process.env.MONGO_PASS;
     const url = process.env.MONGO_URL;
@@ -34,14 +40,33 @@ function connect(){
         debugDB('Info: No user or password provided. Trying to connect without authentication.');
     }
 
+    // Disconnect if already connected or connecting
+    if (mongoose.connection.readyState !== 0) {
+        debugDB('Disconnecting existing MongoDB connection before reconnecting...');
+        try {
+            await mongoose.disconnect();
+            debugDB('Successfully disconnected from MongoDB.');
+        } catch (err) {
+            debugDB('Error disconnecting from MongoDB:', err);
+        }
+    }
+
     debugDB("Trying to connect to MongoDB with "+ user + " at " + url);
     mongoose.connect(url, {
         "authSource": "admin",
         "user": user,
         "pass": password
     })
-        .then(() => {debugDB('Connected to MongoDB('+url+')...'); connected = true})
-        .catch((err: unknown) => {debugDB('Could not connect to MongoDB(' + url + ')...', err); connected = false});
+        .then(() => {
+            debugDB('Connected to MongoDB('+url+')...');
+            connected = true;
+            connecting = false;
+        })
+        .catch((err: unknown) => {
+            debugDB('Could not connect to MongoDB(' + url + ')...', err);
+            connected = false;
+            connecting = false;
+        });
 }
 
 function isConnected(){
